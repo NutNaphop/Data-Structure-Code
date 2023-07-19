b6b80e15f87ac5a1bae210c3ff9fec06e4acd692
Naphop Khumchawna <96785512+NutNaphop@users.noreply.github.com>
2023-07-20 00:54:07 +0700

    Second

--- SingleLinkKist.c
+++ SingleLinkKist.c
@@ -11,10 +11,14 @@ struct node
 
 void insert_first(int val) ; 
 void insert_last(int val) ; 
+void insert_position(int val , int position) ; 
 void display() ;
+
 int main(){  
     int val ;
     int choice ; 
+    int position ; 
+
     while(1){
         printf("\nChoice : ") ; 
         scanf("%d",&choice) ; 
@@ -32,7 +36,16 @@ int main(){
             display() ; 
             break ; 
         case 3: 
+            printf("What position do you want to insert : ") ; 
+            scanf("%d",&position) ; 
+            printf("Number : ") ; 
+            scanf("%d",&val) ; 
+            insert_position(val,position) ; 
+            display() ; 
+            break ; 
+        case 4: 
             exit(0) ; 
+            break ; 
         default:
             printf("Not Found") ; 
             break;
@@ -65,7 +78,20 @@ void insert_last(int val){
     temp -> next = newnode ; 
     
     }
+}
 
+void insert_position(int val , int position){
+    struct node *newnode ; 
+    struct node *temp = head ; 
+    newnode = malloc(sizeof(struct node)) ; 
+    newnode -> data = val ; 
+    for (int i = 1 ; i < position ; i ++){
+        if (temp -> next != NULL){
+            temp = temp -> next ; 
+        }
+    }
+    newnode -> next = temp -> next ;
+    temp -> next = newnode ;  
 
 }
 void display(){
